a8e44c5bac176dbd3c749f4edeec22a7a9c17b6c
adanSmith01 <smith.paredes.13@gmail.com>
2025-02-04 15:49:55 -0300

    refactor: improve code readability and structure

--- QRCodeGenerator.py
+++ QRCodeGenerator.py
@@ -1,27 +1,33 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from PIL import Image
 import qrcode.constants
-import QRCodeInfo
+from QRCodeInfo import QRCodeInfo
 import qrcode
 
 @dataclass
 class QRCodeGenerator():
-    __qrCodeInfo: dict[str, str]
+    __info: str = field(default=None)
+    __imageName: str = field(default=None)
+    __fillColor: str = field(default=None)
+    __backColor: str = field(default=None)
+    __qrCodeInfo: dict[str, str] = field(init=False)
+    logoPath: str = field(default=None)
 
     def __post_init__(self):
-        if not isinstance(self.__qrCodeInfo, dict):
-            raise ValueError("The qr code information is not valid.")
+        qrCodeInfo = QRCodeInfo(self.__info, self.__imageName, self.__fillColor, self.__backColor, self.logoPath)
+        self.__qrCodeInfo = qrCodeInfo.get_info()
         
     
-    def __add_logo(self, logo:Image, imageCode: Image):
+    def __add_logo(self, logoPath:str, imageCode: Image):
+        logo = Image.open(logoPath)
         logo.convert('RGBA')
         imageCode.convert('RGBA')
-
         logo_size = (logo.size[0] // 4, logo.size[1] // 4)
         logo = logo.resize(logo_size, Image.Resampling.LANCZOS)
             
         pos = ((imageCode.size[0] - logo.size[0]) // 2, (imageCode.size[1] - logo.size[1]) // 2)
         imageCode.paste(logo, pos)
+        logo.close()
 
     def generate(self) -> None:
         qr = qrcode.QRCode(
@@ -35,16 +41,12 @@ class QRCodeGenerator():
         qr.make()
         img = qr.make_image(fill_color=self.__qrCodeInfo['fillColor'], back_color=self.__qrCodeInfo['backColor']).get_image()
 
-        if(self.__qrCodeInfo['logo'] is not None):
-            self.__add_logo(self.__qrCodeInfo['logo'], img)
-            self.__qrCodeInfo['logo'].close()
+        if(self.__qrCodeInfo['logoPath'] is not None):
+            self.__add_logo(self.__qrCodeInfo['logoPath'], img)
         
         img.save(self.__qrCodeInfo['imagePath'])
 
 
 if __name__ == '__main__':
-    Logo = Image.open(f"test-logo/icon-dog.png")
-    qrCodeInfo = QRCodeInfo.QRCodeInfo(logo=Logo)
-    qrCodeGenerator = QRCodeGenerator(qrCodeInfo.getInfo())
-    
+    qrCodeGenerator = QRCodeGenerator()
     qrCodeGenerator.generate()
