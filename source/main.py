c15ba57f99afca32d3c92cb0321e8421d14f4444
adanSmith01 <smith.paredes.13@gmail.com>
2025-03-10 23:51:57 -0300

    chore: rename the imports

---
+++ source/main.py
@@ -0,0 +1,17 @@
+from source.qr_code_generator import QRCodeGenerator
+from source.qr_code_info_controller import QRCodeInfoController
+
+
+info: str = QRCodeInfoController.validate_info()
+image_name: str = QRCodeInfoController.validate_image_name()
+fill_color: str = QRCodeInfoController.validate_fill_color()
+back_color: str = QRCodeInfoController.validate_background_color()
+logo_path: str = QRCodeInfoController.validate_logo_path()
+
+
+if logo_path:
+    generator = QRCodeGenerator(info, image_name, fill_color, back_color, logo_path)
+else:
+    generator = QRCodeGenerator(info, image_name, fill_color, back_color)
+
+generator.generate()
